6b0ad96b2461328009ba5f68b021fa024d95ca7c
sacarlson <sacarlson_2000@yahoo.com>
2017-03-11 12:03:49 +0700

    fixed link_install.sh bug

--- link_install.sh
+++ link_install.sh
@@ -1,15 +1,15 @@
-ln -sf  ./upload/admin/controller/extension/payment/stellar_net.php ./admin/controller/extension/payment/stellar_net.php
+ln -srf  ./upload/admin/controller/extension/payment/stellar_net.php ./admin/controller/extension/payment/stellar_net.php
 
-ln -sf ./upload/admin/language_en-gb/extension/payment/stellar_net.php ./admin/language_en-gb/extension/payment/stellar_net.php
+ln -srf ./upload/admin/language/en-gb/extension/payment/stellar_net.php ./admin/language/en-gb/extension/payment/stellar_net.php
 
-#ln -sf ./upload/admin/model/extension/payment/stellar_net.php.not_used_yet ./admin/model/extension/payment/stellar_net.php.not_used_yet
+#ln -srf ./upload/admin/model/extension/payment/stellar_net.php.not_used_yet ./admin/model/extension/payment/stellar_net.php.not_used_yet
 
-ln -sf ./upload/admin/view/template/extension/payment/stellar_net.tpl ./admin/view/template/extension/payment/stellar_net.tpl
+ln -srf ./upload/admin/view/template/extension/payment/stellar_net.tpl ./admin/view/template/extension/payment/stellar_net.tpl
 
-ln -sf ./upload/catalog/controller/extension/payment/stellar_net.php ./catalog/controller/extension/payment/stellar_net.php
+ln -srf ./upload/catalog/controller/extension/payment/stellar_net.php ./catalog/controller/extension/payment/stellar_net.php
 
-ln -sf ./upload/catalog/language/en-gb_extension/payment/stellar_net.php ./catalog/language/en-gb_extension/payment/stellar_net.php
+ln -srf ./upload/catalog/language/en-gb/extension/payment/stellar_net.php ./catalog/language/en-gb/extension/payment/stellar_net.php
 
-ln -sf ./upload/catalog/model/extension/payment/stellar_net.php ./catalog/model/extension/payment/stellar_net.php
+ln -srf ./upload/catalog/model/extension/payment/stellar_net.php ./catalog/model/extension/payment/stellar_net.php
 
-ln -sf ./upload/catalog/view/theme/default/template/extension/payment/stellar_net.tpl ./catalog/view/theme/default/template/extension/payment/stellar_net.tpl
+ln -srf ./upload/catalog/view/theme/default/template/extension/payment/stellar_net.tpl ./catalog/view/theme/default/template/extension/payment/stellar_net.tpl
--- make_quickview_links.sh
+++ make_quickview_links.sh
@@ -1,15 +1,23 @@
-ln -sf ./upload/admin/controller/extension/payment/stellar_net.php ./quickview_links/admin_controller_extension_payment/stellar_net.php
+mkdir -p ./quickview_links/admin_controller_extension_payment
+ln -srf ./upload/admin/controller/extension/payment/stellar_net.php ./quickview_links/admin_controller_extension_payment/stellar_net.php
 
-ln -sf ./upload/admin/language_en-gb/extension/payment/stellar_net.php ./quickview_links/admin_language_en-gb_extension_payment/stellar_net.php
+mkdir -p ./quickview_links/admin_language_en-gb_extension_payment
+ln -srf ./upload/admin/language_en-gb/extension/payment/stellar_net.php ./quickview_links/admin_language_en-gb_extension_payment/stellar_net.php
 
-ln -sf ./upload/admin/model/extension/payment/stellar_net.php.not_used_yet ./quickview_links/admin_model_extension_payment/stellar_net.php.not_used_yet
+#mkdir -p ./quickview_links/
+#ln -srf ./upload/admin/model/extension/payment/stellar_net.php.not_used_yet ./quickview_links/admin_model_extension_payment/stellar_net.php
 
-ln -sf ./upload/admin/view/template/extension/payment/stellar_net.tpl ./quickview_links/admin_view_template_extension_payment/stellar_net.tpl
+mkdir -p ./quickview_links/admin_view_template_extension_payment
+ln -srf ./upload/admin/view/template/extension/payment/stellar_net.tpl ./quickview_links/admin_view_template_extension_payment/stellar_net.tpl
 
-ln -sf ./upload/catalog/controller/extension/payment/stellar_net.php ./quickview_links/catalog_controller_extension_payment/stellar_net.php
+mkdir -p ./quickview_links/catalog_controller_extension_payment
+ln -srf ./upload/catalog/controller/extension/payment/stellar_net.php ./quickview_links/catalog_controller_extension_payment/stellar_net.php
 
-ln -sf ./upload/catalog/language/en-gb_extension/payment/stellar_net.php ./quickview_links/catalog_language_en-gb_extension_payment/stellar_net.php
+mkdir -p ./quickview_links/catalog_language_en-gb_extension_payment
+ln -srf ./upload/catalog/language/en-gb_extension/payment/stellar_net.php ./quickview_links/catalog_language_en-gb_extension_payment/stellar_net.php
 
-ln -sf ./upload/catalog/model/extension/payment/stellar_net.php ./quickview_links/catalog_model_extension_payment/stellar_net.php
+mkdir -p ./quickview_links/catalog_model_extension_payment
+ln -srf ./upload/catalog/model/extension/payment/stellar_net.php ./quickview_links/catalog_model_extension_payment/stellar_net.php
 
-ln -sf ./upload/catalog/view/theme/default/template/extension/payment/stellar_net.tpl ./quickview_links/catalog_view_theme_default_template_extension_payment/stellar_net.tpl
+mkdir -p ./quickview_links/catalog_view_theme_default_template_extension_payment
+ln -srf ./upload/catalog/view/theme/default/template/extension/payment/stellar_net.tpl ./quickview_links/catalog_view_theme_default_template_extension_payment/stellar_net.tpl
